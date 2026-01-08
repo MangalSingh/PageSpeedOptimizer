e9946cb439581980d79825398b849f4ecd748431
Mangal Singh <mangalsingh16ptd@gmail.com>
2026-01-08 12:50:41 +0530

    Deleted all files

--- Octocub/PageSpeedOptimizer/registration.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Octocub_PageSpeedOptimizer',
-    __DIR__
-);
--- Octocub/PageSpeedOptimizer/view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-  map: {
-    '*': {
-      'octocub_gps/lazy/io': 'Octocub_PageSpeedOptimizer/js/lazy/io',
-      'octocub_gps/lazy/io_lqip': 'Octocub_PageSpeedOptimizer/js/lazy/io_lqip',
-      'octocub_gps/lazy/idle': 'Octocub_PageSpeedOptimizer/js/lazy/idle'
-    }
-  }
-};
